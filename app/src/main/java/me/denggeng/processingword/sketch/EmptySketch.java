f69b77c5ccd0fd16ad002e47e66b48ffb2a12d12
denggeng <251314718@qq.com>
2017-06-13 19:13:20 +0800

    add some basic codes

---
+++ app/src/main/java/me/denggeng/processingword/sketch/EmptySketch.java
@@ -0,0 +1,18 @@
+package me.denggeng.processingword.sketch;
+
+import android.app.Activity;
+
+import me.denggeng.processingword.MainActivity;
+import processing.core.PApplet;
+
+/**
+ * Created by root on 23/6/15.
+ */
+public class EmptySketch extends PApplet {
+
+    @Override
+    public void onAttach(Activity activity) {
+        super.onAttach(activity);
+        ((MainActivity) activity).onSectionAttached(1);
+    }
+}
